491ebef6625f6274b342942b2d815db321f2a20c
nibaldo <nibaldo.donoso@cosmotech.com>
2025-01-22 17:34:52 +0100

    add workflows and readme

--- variables.app.tf
+++ variables.app.tf
@@ -1,14 +1,14 @@
 variable "azure_tenant_id" {
-  type        = string
+  type = string
 }
 variable "azure_subscription_id" {
   type = string
 }
 variable "azure_client_id" {
-  type        = string
+  type = string
 }
 variable "azure_client_secret" {
-  type        = string
+  type = string
 }
 variable "audience" {
   description = "The App Registration audience type"
